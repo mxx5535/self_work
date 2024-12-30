b7dbf310cc40228f053406fbef6cfab1e55de959
李金泽 <9378762+lijinze1@user.noreply.gitee.com>
2024-12-30 15:49:43 +0800

    本地模型部署

---
+++ main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time : 2024/12/29 18:36
+# @Author : lijinze
+# @Email : lijinze@lzzg365.cn
+# @File : main.py
+# @Project : self_work
+from tools.express_tools import search_express_tool
